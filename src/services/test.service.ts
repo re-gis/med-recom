7ba3725c2ae0e7762ae0569b157ee70469b96c9b
re-gis <irumvaregisdmc@gmail.com>
2025-01-09 12:04:38 +0200

    [++]: encapsulation

--- src/services/test.service.ts
+++ src/services/test.service.ts
@@ -1,28 +1,30 @@
 import axios from "axios";
 import { dotenv } from "../utils/dotenv";
-import { EDITION, SNOMED_CT_API_URL, VERSION } from "../constants/constant";
+import {
+  LOINC_API_PASS,
+  LOINC_API_URL,
+  LOINC_API_USERNAME,
+} from "../constants/constant";
 
 dotenv();
 
 export default class TestService {
   static async fetchTests(query: string) {
-    const username = "dregis";
-    const password = "0786430853Dmc!";
-    const credentials = Buffer.from(`${username}:${password}`).toString(
-      "base64"
-    );
-    const url = `https://loinc.regenstrief.org/searchapi/loincs?query=${query}`; 
+    const credentials = Buffer.from(
+      `${String(LOINC_API_USERNAME)}:${String(LOINC_API_PASS)}`
+    ).toString("base64");
+    const url = `${String(LOINC_API_URL)}?query=${query}`;
     try {
       const response = await axios.get(url, {
         headers: {
-          "Authorization": `Basic ${credentials}`,
-          "Accept":"application/json"
-        }
+          Authorization: `Basic ${credentials}`,
+          Accept: "application/json",
+        },
       });
-      return response.data; 
+      return response.data;
     } catch (error) {
       console.error("Error fetching FHIR data", error);
-      throw error; 
+      throw error;
     }
   }
 }
